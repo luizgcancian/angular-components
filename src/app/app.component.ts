0d8edbba805e7ddc7ac19f909b2549df45ea4531
luizgcancian <luizgnr@hotmail.com>
2023-05-18 01:32:15 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: '@nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: '@mountainLover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/biking.jpeg',
+      username: '@biking122',
+      content: 'I saw this bike today',
+    },
+  ];
 }
